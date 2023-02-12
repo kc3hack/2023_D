5e69ac51717979e7eece78f368f8025345b61d46
forest-book <hervarar51@outlook.jp>
2023-02-12 22:54:51 +0900

    roomcreate部分の改修

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 import pytz
 from datetime import datetime
 import uuid
-#import roomcreate
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///development.db'
 app.config['SQLALCHEMY_ECHO'] = True
+
 
 # sessionを使う際にSECRET_KEYを設定
 app.config['SECRET_KEY'] = 'secret_key'  # TODO 後で変える
@@ -20,14 +21,16 @@ login_manager.init_app(app)
 
 
 class User(UserMixin, db.Model):
-   id = db.Column(db.Integer, primary_key=True)
-   name = db.Column(db.String, nullable=False,unique=True)
-   password = db.Column(db.String, nullable=False)
-   # フロント側でユーザを識別する
-   uuid = db.Column(db.String, nullable=False, unique=True)
-   # ユーザが登録された時の時間
-   created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
-   room = db.relationship("Room", uselist=False, back_populates="user",  cascade="all, delete, delete-orphan")
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    # フロント側でユーザを識別する
+    uuid = db.Column(db.String, nullable=False, unique=True)
+    # ユーザが登録された時の時間
+    created_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.now(pytz.timezone('Asia/Tokyo')))
+    room = db.relationship(
+        "Room", uselist=False, back_populates="user",  cascade="all, delete, delete-orphan")
 
 
 @app.route('/register', methods=['POST'])
@@ -49,7 +52,7 @@ def register():
         password=userpassword,
         uuid=useruuid,
         created_at=usercreated_at
-        )
+    )
 
     # エラーが出たらロールバック
     try:
@@ -62,14 +65,18 @@ def register():
         return 'False,' + str(type(e).__name__)
 
 # 分割予定
+
+
 class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     room_number = db.Column(db.Integer, nullable=False, unique=True)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"),nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.now(pytz.timezone('Asia/Tokyo')))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False,unique=True)
     user = db.relationship("User", back_populates="room")
 
-@app.route('/roomcreate',methods=['POST'])
+
+@app.route('/roomcreate', methods=['POST'])
 def roomcreate():
     # jsonリクエストから値取得
     json = request.get_json()
@@ -81,34 +88,22 @@ def roomcreate():
     room_number = data['room_number']
     user_uuid = data['user_uuid']
 
-    
-    # カラムで絞り込み
-    if (user_uuid==''):
+    user = User.query.filter_by(uuid=user_uuid).first()
+    if(user==None):
         return 'False,' + '不正なユーザーです'
     else:
-        user = User.query.filter_by(uuid=user_uuid).first()
-       
-
-    
-
         room = Room(
             room_number=room_number,
             created_at=datetime.now(pytz.timezone('Asia/Tokyo')),
             user_id=user.id
             )
-
+    
         # エラーが出たらロールバック
         try:
             # dbにroomを登録
             db.session.add(room)
             db.session.commit()
-            return 'True' 
+            return 'True'
         except Exception as e:
             db.session.rollback()
             return 'False,' + str(type(e).__name__)
-
-        
-
-
-
-
