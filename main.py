34f418078666d630dae2752c063f702739c20d6b
krn-7717 <104199555+krn-7717@users.noreply.github.com>
2023-02-13 22:31:57 +0900

    pinを作成する機能

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ class User(UserMixin, db.Model):
     # ユーザが登録された時の時間
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
     room = db.relationship("Room", uselist=False, back_populates="user",  cascade="all, delete, delete-orphan")
+    pin = db.relationship("Pin", uselist=False, back_populates="user",  cascade="all, delete, delete-orphan")
 
 def get_json():
     # jsonリクエストから値取得
@@ -171,10 +172,64 @@ def roomdelete():
     
     else:
         return 'False,オーナーではありません'
-    
-    
-    
-        
 
-        
+# 分割予定
+class Pin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    altitude = db.Column(db.Float, nullable=False)
+    longuitude = db.Column(db.Float, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    type = db.Column(db.String, nullable=False)
+    uuid = db.Column(db.String, nullable=False, unique=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False,unique=True)
+    user = db.relationship("User", back_populates="pin")
+
+@app.route('/pinadd', methods=['POST'])
+def pinadd():
+    data = get_json()
+    user_uuid = data['user_uuid']
+    longuitude = data['longuitude']
+    latitude = data['latitude']
+    altitude = data['altitude']
+    pin_type = data['pin_type']
+
+    # uuidを生成
+    pin_uuid = str(uuid.uuid4())
+
+    user = User.query.filter_by(uuid=user_uuid).first()
+    if(user==None):
+        return 'False,不正なユーザーです'
+
+    typelist = ['go', 'enemy'] # TODO typeを追加
+    if(pin_type not in typelist): 
+        return 'False,不正なピンです'
+
+    pin = Pin(
+        altitude=altitude,
+        longuitude=longuitude,
+        latitude=latitude,
+        type=pin_type,
+        uuid=pin_uuid,
+        created_at=datetime.now(pytz.timezone('Asia/Tokyo')),
+        user_id=user.id,
+        )
     
+    if(user.pin != None):
+        try:
+            db.session.delete(user.pin)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return 'False,' + str(type(e).__name__)
+
+    # エラーが出たらロールバック
+    try:
+        # dbにpinを登録
+        db.session.add(pin)
+        db.session.commit()
+        return 'True'
+    except Exception as e:
+        db.session.rollback()
+        return 'False,' + str(type(e).__name__)
