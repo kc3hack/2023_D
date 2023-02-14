f0a08257e396fe2928a53379ae4eb969add08b5e
kawabata324 <kawabataharuki48@gmail.com>
2023-02-15 00:10:10 +0900

    [Update] RoomPolingの完成

--- app/models/room_member.py
+++ app/models/room_member.py
@@ -6,7 +6,7 @@ import pytz
 class RoomMember(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     room_id = db.Column(db.Integer, db.ForeignKey("room.id"), nullable=False)
-    room = db.relationship("Room", back_populates="room_member")
+    room = db.relationship("Room", back_populates="room_members")
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False, unique=True)
     user = db.relationship("User", back_populates="room_member")
