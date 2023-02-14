b232e47d3ae0ee21c649ed83bac5c5e76312f275
forest-book <124020491+forest-book@users.noreply.github.com>
2023-02-14 23:46:30 +0900

    Reaction機能の実装

---
+++ app/models/reaction.py
@@ -0,0 +1,15 @@
+from app.core import db
+from datetime import datetime
+import pytz
+
+
+class Reaction(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey("user.id"),nullable=False)
+    user = db.relationship("User",back_populates="reactions")
+    pin_id = db.Column(db.Integer,db.ForeignKey("pin.id"),nullable=False)
+    pin = db.relationship("Pin",back_populates="reactions")
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+    content = db.Column(db.String)
+    
+    
