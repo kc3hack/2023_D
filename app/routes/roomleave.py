552f7e50796e050389a1c0b369c44379a146c3c5
kawabata324 <kawabataharuki48@gmail.com>
2023-02-14 22:44:31 +0900

    [Update] roomleave APIの完成

--- app/routes/roomleave.py
+++ app/routes/roomleave.py
@@ -15,8 +15,6 @@ def roomleave():
 
     user = User.query.filter_by(uuid=user_uuid).first()
 
-    room_members = user.room_member
-
     # 不正なユーザーのとき
     if (user == None):
         return 'False,NOT found user'
@@ -26,6 +24,7 @@ def roomleave():
         return 'False,delete room for leaving this room'
 
     # オーナーではない(RoomMenberであるとき)
+    room_members = user.room_member
     if (room_members != None):
         # エラーが出たらロールバック
         try:
@@ -37,6 +36,6 @@ def roomleave():
             db.session.rollback()
             return 'False,' + str(type(e).__name__)
 
-    #ユーザー登録はされているがオーナーでもメンバーでもないとき
+    # ユーザー登録はされているがオーナーでもメンバーでもないとき
     else:
         return 'False,Caution! unexpected commit'
