09ba5eeff4645609b58abffc6181d671907bcc8f
forest-book <124020491+forest-book@users.noreply.github.com>
2023-02-15 20:33:22 +0900

    pincancelの実装

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from app.routes.pinadd import blueprint as pinadd_blueprint
 from app.routes.roompoling import blueprint as roompoling_blueprint
 from app.routes.roomleave import blueprint as roomleave_blueprint
 from app.routes.reactionadd import blueprint as reactionadd_blueprint
+from app.routes.pincancel import blueprint as pincancel_blueprint
 
 app.register_blueprint(register_blueprint)
 app.register_blueprint(login_blueprint)
@@ -18,3 +19,6 @@ app.register_blueprint(pinadd_blueprint)
 app.register_blueprint(roompoling_blueprint)
 app.register_blueprint(roomleave_blueprint)
 app.register_blueprint(reactionadd_blueprint)
+app.register_blueprint(pincancel_blueprint)
+
+
